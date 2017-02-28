ef7e872d3583705b69de26a8ebc8c31556b2149f
Samuel Davis <samueljakdavis@gmail.com>
2017-02-28 09:28:52 -0500

    Recursively read a given directory for .json files which can be compiled into a map

--- lib/tagcursion.ex
+++ lib/tagcursion.ex
@@ -4,22 +4,35 @@ defmodule Tagcursion do
   """
 
   @doc """
-  Read a list of entities into memory from `filepath`
+  Collect a list of tags into a map from the `data_dir` directory path
   """
-  def read_json(filepath) do
-    filepath
-    |> File.read!
-    |> Poison.decode!(as: [%{}])
-    |> Enum.into(%{}, &({&1["id"], &1}))
+  def read_json_dir(data_dir \\ "data/") do
+    data_dir
+    |> File.ls!
+    |> Stream.map(fn filename -> path = data_dir <> filename
+      cond do
+        File.dir?(path) -> read_json_dir(path <> "/")
+        Regex.match?(~r/\.json$/, path) -> File.read!(path) |> Poison.decode!(as: [%{}])
+        true -> []
+      end
+    end)
+    |> Enum.concat
   end
-  
+
+  @doc """
+  Read a `tag_list` into a map, keyed by "id"
+  """
+  def tag_list_to_map(tag_list) do
+    Enum.into(tag_list, %{}, &({&1["id"], &1}))
+  end
+
   def reduce_tags(tag_store, tag, acc \\ [])
 
   @doc """
   Collect a list of tags from a tag's related tag_ids
   """
   def reduce_tags(tag_store, tag, acc) when is_map(tag),
-    do: reduce_tags(tag_store, Map.get(tag, "tags", []), acc)
+  do: reduce_tags(tag_store, Map.get(tag, "tags", []), acc)
 
   @doc """
   Collect a list of tags from a list of `tag_ids`
