612e675ced9b32b59434666415f683e92fcedd01
Samuel Davis <samueljakdavis@gmail.com>
2017-03-06 16:26:30 -0500

    Support for semicolon-delineated lists

--- lib/cli.ex
+++ lib/cli.ex
@@ -1,21 +1,25 @@
 defmodule Tagcursion.Cli do
   def build_tag do
     id = String.trim(IO.gets("Id: "))
+    name = String.trim(IO.gets("Name: "))
+    description = String.trim(IO.gets("Description: "))
     tags = String.split(IO.gets("Associated Tags: "), ",")
     |> Enum.map(&String.trim/1)
     |> Enum.reject(&(&1 == ""))
     props = read_props()
+    props = Map.put(props, "description", description)
+    props = Map.put(props, "name", name)
 
     Tagcursion.hydrate({id, props, tags})
   end
 
   defp read_props(props \\ %{}) do
-    case String.split(IO.gets("Prop: "), "=") do
-      ["done\n"] -> props
-      [prop, value] -> props
-      |> Map.put(String.trim(prop), String.trim(value))
-      |> read_props
-      _ -> read_props(props)
+    case String.split(IO.gets("Prop: "), "=") |> Enum.map(&String.trim/1) do
+      [prop, value] -> cond do
+        String.contains?(value, ";") -> Map.put(props, prop, String.split(value, ";")) |> read_props
+        true -> Map.put(props, prop, value) |> read_props
+      end
+      _ -> props
     end
   end
 end
